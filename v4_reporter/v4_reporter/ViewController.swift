27d36ebe6cdc4ccf197561de9690dc7b6689fac1
Evgeniy <evgver@bk.ru>
2016-10-12 00:01:52 +0300

    Add files via upload

---
+++ v4_reporter/v4_reporter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  v4_reporter
+//
+//  Created by Yevhenii Veretennikov on 09.10.16.
+//  Copyright © 2016 Yevhenii Veretennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+    }
+}
+
